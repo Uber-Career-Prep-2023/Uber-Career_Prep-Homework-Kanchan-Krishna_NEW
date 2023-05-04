127b550864aa6a0c555f747c3c01b0ce6e99541a
kanchankrishna <46735595+kanchankrishna@users.noreply.github.com>
2023-05-04 18:46:55 -0500

    Added test cases for Q5

--- Assignment2/Q5isBST.cpp
+++ Assignment2/Q5isBST.cpp
@@ -1,24 +1,25 @@
 #include <bits/stdc++.h>
+class Node {
+    public:
+        Node* left;
+        Node* right;
+        int data;
+        Node(int data) {
+            this->data = data;
+            this->left = nullptr;
+            this->right = nullptr;
+        }
+    private:
+        Node* root;
+};
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
-// TC & SC: O(n) esp when you have a skewed tree like 4-3-2-1 to the left and recursive stack is going to store all these values
+// TC & SC: O(N) where N is the # of nodes in the tree esp when you have a skewed tree like 4-3-2-1 to the left and recursive stack is going to store all these values
 class Solution { // top down dfs
 public:
-    bool isValidBST(TreeNode *root) {
+    bool isValidBST(Node* root) {
         return validHelper(root, nullptr, nullptr); //-inf and +inf
     }
-    bool validHelper(TreeNode *root, TreeNode *low, TreeNode *high) {
+    bool validHelper(Node* root, Node* low, Node* high) {
         if (root == nullptr) {
             return true; // empty tree is a valid BST
         }
@@ -30,3 +31,14 @@ public:
         // moving on to right subtree: high limit is same; low changes to prev root
     }
 };
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(3);
+    root->right = new Node(7);
+    root->left->left = new Node(4);
+    root->left->right = new Node(6);
+    root->right->left = new Node(9);
+    root->right->right = new Node(1);
+    cout << isValidBST(root) << endl;
+}
