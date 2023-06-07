658decba198caf18f04a71604c733731b87aacc1
Kanchan Krishna <46735595+kanchankrishna@users.noreply.github.com>
2023-06-07 14:34:35 -0700

    Brute forcing Q5

---
+++ Assignment3/Q5FirstKBinaryNumbers.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+
+//Given a number, k, return an array of the first k binary numbers, represented as strings.
+/*
+The brute force way is to use a series of helper methods; iterate until and including k; call a method to generate the binary version of that number k; convert it to a string; push it back to a vector 
+*/
