786d7cd252e368d8c8630f692c0762055a3262a6
Kanchan Krishna <kanchankrishna1063@gmail.com>
2023-07-25 18:20:31 -0700

    Added Running Median

--- Assignment4/Q3RunningMedian.cpp
+++ Assignment4/Q3RunningMedian.cpp
@@ -4,8 +4,24 @@ using namespace std;
 //You will be given a stream of numbers, one by one. After each new number, 
 //return the median of the numbers so far.
 
+int findMiddleNum(priority_queue<int, vector<int>, greater<int>>& min_heap) {
+    int size = min_heap.size(); 
+    int mid = size / 2; 
+    int i = 0;
+    while (i < mid) {
+        min_heap.pop();
+        i++;
+    }
+    return min_heap.front();
+}
+
 int runningMedian(vector<int>& nums, int start, int end) {
     //constantly sort in ascending order -> maintain a minimum heap
+    priority_queue<int, vector<int>, greater<int>> min_heap; 
+    for (int i = start; i <= end; i++) {
+        pq.push(nums[i]);
+    }
+    return findMiddleNum(min_heap)
 }
 
 int main() {
---
+++ Assignment4/Q8CoinChange.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Given a list of coin denominations and a target sum, return the number of possible ways to 
+//make change for that sum.
+
+int coinDenoms(vector<int>& coins, int target) {
+    int count = 0;
+    return count; 
+}
+
+int main() {
+
+}
