f88c339a39fc94eb92897bfde3a668509aef8227
Kanchan Krishna <kanchankrishna1063@gmail.com>
2023-07-26 12:08:56 -0700

    Added coin change

--- Assignment4/Q8CoinChange.cpp
+++ Assignment4/Q8CoinChange.cpp
@@ -4,11 +4,24 @@ using namespace std;
 //Given a list of coin denominations and a target sum, return the number of possible ways to 
 //make change for that sum.
 
-int coinDenoms(vector<int>& coins, int target) {
-    int count = 0;
-    return count; 
+int coinChange(int coins[], int num_coins, int target_sum) {
+    if (target_sum == 0) {
+        return 1;
+    }
+    if (target_sum < 0) {
+        return 0;
+    }
+    if (num_coins <= 0) {
+        return 0;
+    }
+    return coinChange(coins, num_coins - 1, target_sum) + coinChange(coins, num_coins, target_sum - coins[num_coins - 1]);
 }
 
 int main() {
-
+    int coins[] = {2, 5, 10};
+    int length = sizeof(coins) / sizeof(coins[0]);
+    int target_sum = 20;
+    cout << " " << count(coins, length, target_sum);
+    target_sum = 15;
+    cout << " " << count(coins, length, target_sum);
 }
